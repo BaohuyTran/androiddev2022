47afbf2eb75d2888863b44ca4202f0633d1a4c77
BaohuyTran <baohuyt11@gmail.com>
2021-10-19 01:46:46 +0700

    lw6

--- app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
+++ app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
@@ -11,15 +11,7 @@ public class WeatherActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weather);
-//        ForecastFragment ff = ForecastFragment.newInstance("", "");
-//        // Add the fragment to the 'container' FrameLayout
-//        getSupportFragmentManager().beginTransaction().add(
-//                R.id.container, ff).commit();
         Log.i("Weather", "onCreate() called");
-
-        ForecastFragment ff = new ForecastFragment();
-        getSupportFragmentManager().beginTransaction().replace(
-                R.id.container, ff).commit();
     }
 
     @Override
